0f15aefe163d076aa484607fd4a995e8e4434505
thordur <thordurka@vedur.is>
2020-11-25 22:15:39 +0000

    initial commit

---
+++ tremv_back.py
@@ -0,0 +1,148 @@
+import cherrypy
+import os
+import sys
+import datetime
+import tremv_common as common
+
+class backend(object):
+    def __init__(self):
+        self.config_stamp = 0
+        self.config_filename = "tremv_config.json"
+        self.config = {}
+
+    def reload_config(self):
+        stamp = os.stat(self.config_filename).st_mtime
+
+        if(stamp != self.config_stamp):
+            self.config = common.read_tremv_config(self.config_filename)
+            self.config_stamp = stamp
+        
+        return(self.config)
+
+    @cherrypy.expose
+    def index(self):
+        return "hello!"
+
+
+    """Return the most up to date list of stations as json.
+    """
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def station_names(self):
+        self.reload_config()
+
+        return(self.config["station_names"])
+    
+
+    """Return the most up to date list of filters as json.
+    """
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def filters(self):
+        self.reload_config()
+
+        return(self.config["filters"])
+
+
+    """ Reads the newest tremvlogs based on provided filters, and returns the latest
+        values for each station that was asked for.
+    """
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def latest(self):
+        result = []
+        query = cherrypy.request.json
+
+        self.reload_config()
+
+        station_names = self.config["station_names"]
+        filters = self.config["filters"]
+
+        if("station_names" in query):
+            if(len(query["station_names"]) > 0):
+                station_names = query["station_names"]
+
+        if("filters" in query):
+            if(len(query["filters"]) > 0):
+                filters = query["filters"]
+
+        #NOTE: Reading the entire file in for now. Prehaps this can be made more efficient via seeking in the file or something...
+        date = datetime.datetime.now()
+        folder_path = common.generate_output_path(date)
+
+        for f in filters:
+            if(f in self.config["filters"]):
+                tremvlog_filename = common.generate_tremvlog_filename(date, f)
+                path = folder_path + tremvlog_filename
+                rsam_results = common.read_tremvlog_file(path, station_names)
+
+                station_data = {}
+
+                for name in station_names:
+                    if(name in self.config["station_names"]):
+                        station_data[name] = rsam_results[name][-1]
+                    else:
+                        station_data[name] = 0.0
+
+                result.append({"filter": f, "stations": station_data})
+
+        return(result)
+
+
+    """ Reads tremvlogs based on provided date and filters, and returns the stations
+        as a json.
+    """
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def date(self):
+        result = []
+        query = cherrypy.request.json
+
+        self.reload_config()
+
+        station_names = self.config["station_names"]
+        filters = self.config["filters"]
+
+        if("station_names" in query):
+            if(len(query["station_names"]) > 0):
+                station_names = query["station_names"]
+
+        if("filters" in query):
+            if(len(query["filters"]) > 0):
+                filters = query["filters"]
+
+        date = datetime.datetime(query["date"]["year"], query["date"]["month"], query["date"]["day"])
+        minute_of_day = (date.hour * 60) + date.minute
+
+        folder_path = common.generate_output_path(date)
+
+        for f in filters:
+            if(f in self.config["filters"]):
+                tremvlog_filename = common.generate_tremvlog_filename(date, f)
+                path = folder_path + tremvlog_filename
+                rsam_results = common.read_tremvlog_file(path, station_names)
+
+                station_data = {}
+
+                for name in station_names:
+                    if(name in self.config["station_names"]):
+                        station_data[name] = rsam_results[name]
+                    else:
+                        station_data[name] = [0.0 for x in range(0, minute_of_day)]
+
+                result.append({"filter": f, "stations": station_data})
+
+        return(result)
+
+#TODO: support querying for an arbritrary date range, not just a specific date
+
+if(__name__ == "__main__"):
+    if(len(sys.argv) == 1):
+        print("Server port argument is required.")
+        sys.exit()
+
+    cherrypy.server.socket_host = "0.0.0.0"
+    cherrypy.server.socket_port = int(sys.argv[1])
+    cherrypy.quickstart(backend())
---
+++ tremv_daemon.py
@@ -0,0 +1,178 @@
+import os
+import random
+import time
+import obspy
+from obspy.clients.seedlink.basic_client import Client
+from obspy import UTCDateTime
+import tremv_common as common
+
+
+""" Returns a list of station names from obspy trace object.
+"""
+def list_station_names(stations):
+    result = []
+
+    for trace in stations:
+        result.append(trace.stats["station"])
+
+    return(result)
+
+
+""" Apply lowpass filter to the data and downsample it from 100 points per minute
+    to 20 per minute.
+"""
+def process_station_data(stations):
+    result = stations.copy()
+
+    #This is done in order to avoid the phase shift in the decimation process
+    result.filter('lowpass', freq=20.0, corners=2, zerophase=True)#this is super slow!
+ 
+    #down sample from 100hz to 20hz (100/5 = 20), no filter to avoid phase shift
+    for trace in result:
+        trace.decimate(5, no_filter=True)
+ 
+    result.detrend("demean")
+
+    return(result)
+
+
+""" Iterate through each filter, applys it to each trace and adds the filtered
+    trace to a list corresponding to the filter that the iteration is on.
+"""
+def apply_bandpass_filters(traces, filters):
+    passbands = [{} for i in filters]
+ 
+    for i in range(0, len(filters)):
+        f = filters[i]
+ 
+        for trace in traces:
+            t = trace.copy()
+            t.filter("bandpass", freqmin=float(f[0]), freqmax=float(f[1]), corners=4, zerophase=True)
+            passbands[i][t.stats["station"]] = t
+
+    return(passbands)
+
+
+def trace_average(trace):
+    """ Applies rsam to a single trace.
+        NOTE: This function assumes the given trace is 1 minute worth of data.
+    """
+    value_sum = 0
+    pts_per_minute = int(trace.stats.sampling_rate * 60)
+
+    for point in trace.data:
+        value_sum += abs(point)
+
+    return(value_sum/pts_per_minute)
+
+
+def rsam_processing(per_filter_filtered_stations, filters, station_names, received_station_names):
+    result = [{} for i in filters]
+
+    for i in range(0, len(filters)):
+        for name in station_names:
+            result[i][name] = 0.0
+
+    #here we accumulate points
+    for i in range(0, len(filters)):
+        station_rsam_dict = result[i]
+ 
+        for name in station_names:
+            if(name in received_station_names):
+                trace = per_filter_filtered_stations[i][name]
+                average = trace_average(trace)
+                station_rsam_dict[name] = average
+            else:
+                station_rsam_dict[name] = 0.0
+
+    return(result)
+
+
+def write_tremvlog_file(rsam_results, filters, station_names, starttime):
+    """ Creates output files...
+    """
+    print("writing to file...")
+    starttime_datetime = starttime.datetime
+    path = common.generate_output_path(starttime_datetime)
+
+    if(os.path.exists(path) == False):
+        os.makedirs(path)
+
+    for i in range(0, len(filters)):
+        filename = common.generate_tremvlog_filename(starttime_datetime, filters[i])
+        file_path = path + filename
+        file_exists = os.path.exists(file_path)
+
+        output = open(file_path, "a")
+ 
+        if(file_exists):
+            output.write("\n" + starttime_datetime.isoformat() + "\n")
+ 
+            for name in station_names:
+                output.write(str(rsam_results[i][name])+" ")
+        else:
+            for name in station_names:
+                output.write(str(name)+" ")
+ 
+            output.write("\n"+ starttime_datetime.isoformat() +"\n")
+
+            for name in station_names:
+                output.write(str(rsam_results[i][name])+" ")
+ 
+        output.close()
+    print("wrote to tremvlog!")
+    print()
+
+
+def main():
+    config_filename = "tremv_config.json"
+    config = common.read_tremv_config(config_filename)
+    config_stamp = os.stat(config_filename).st_mtime
+
+    seedlink_connection = Client(config["server"], config["port"], 5, False)
+    filters = config["filters"]
+
+    SEC_TO_NANO = 1000*1000*1000
+    min_in_ns = 60 * SEC_TO_NANO
+
+    while(True):
+        print("Sleeping until next minute.")
+
+        #NOTE:  This makes it so the sleep time is the duration from now to the next minute.
+        #       Thus calculations happen every minute, according to the system clock(assuming calculations take less than a minute).
+        #       Would use time.time_ns() but it is only available in python 3.7 and up.
+        sleeptime_in_sec = (min_in_ns - (int(time.time() * SEC_TO_NANO) % min_in_ns)) / SEC_TO_NANO
+        time.sleep(sleeptime_in_sec)
+
+        starttime = UTCDateTime()
+        print("Fetching data...")
+        #TODO:Maybe the station parameter(the one after "VI") could be longer than 3 chars???
+        received_stations = seedlink_connection.get_waveforms(config["network"], "???", "??", "HHZ", starttime - 60, starttime)
+        print("Fetch duration: " + str(UTCDateTime() - starttime))
+        print()
+
+        station_names = config["station_names"]
+
+        rsam_st = UTCDateTime()
+
+        received_station_names = list_station_names(received_stations)
+        pre_processed_stations = process_station_data(received_stations)
+        per_filter_filtered_stations = apply_bandpass_filters(pre_processed_stations, filters)
+
+        rsam_results = rsam_processing(per_filter_filtered_stations, filters, station_names, received_station_names)
+
+        write_tremvlog_file(rsam_results, filters, station_names, starttime)
+
+        print("Rsam calculation duration: " + str(UTCDateTime() - rsam_st))
+        print("Total duration: " + str(UTCDateTime() - starttime))
+        print()
+
+        #reload the config file if it has changed
+        stamp = os.stat(config_filename).st_mtime
+
+        if(stamp != config_stamp):
+            config = common.read_tremv_config(config_filename)
+            config_stamp = stamp
+
+#TODO: run process as daemon?
+main()
