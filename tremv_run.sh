c4059924acf72fd13aff2c93fdd4e60044d841d1
thordur <thordurka@vedur.is>
2020-12-03 16:40:47 +0000

    fix

--- tremv_run.sh
+++ tremv_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-if ! screen -list | grep -q "tremv_logger"; then
-	screen -S tremv_logger -dm python3 tremv_logger.py
-fi
-if ! screen -list | grep -q "tremv_server"; then
-	screen -S tremv_server -dm python3 tremv_server.py
+if (( $# != 1 )); then
+	echo "you need to provide a server port"
+else
+	if ! screen -list | grep -q "tremv_logger"; then
+		screen -S tremv_logger -dm python3 tremv_logger.py
+	fi
+	if ! screen -list | grep -q "tremv_server"; then
+		screen -S tremv_server -dm python3 tremv_server.py $1
+	fi
 fi
