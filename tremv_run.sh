ac93f62382a4d2818fc99735c8c038ab3c29a939
Thordur Agust <ThordurKa@vedur.is>
2021-08-05 14:37:52 +0000

    added port as an id for the screen instances

--- tremv_run.sh
+++ tremv_run.sh
@@ -2,10 +2,10 @@
 if (( $# != 1 )); then
 	echo "you need to provide a server port"
 else
-	if ! screen -list | grep -q "tremv_logger"; then
-		screen -S tremv_logger -dm python3 tremv_logger.py
+	if ! screen -list | grep -q "tremv_logger$1"; then
+		screen -S "tremv_logger$1" -dm python3 tremv_logger.py
 	fi
-	if ! screen -list | grep -q "tremv_server"; then
-		screen -S tremv_server -dm python3 tremv_server.py $1
+	if ! screen -list | grep -q "tremv_server$1"; then
+		screen -S "tremv_server$1" -dm python3 tremv_server.py $1
 	fi
 fi
