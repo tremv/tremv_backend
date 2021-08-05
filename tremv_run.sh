7c10e110f42ead9d76dfa3a923b1506c14c2062d
tremv <tremv@tremor.is>
2021-08-05 14:42:52 +0000

    typo

--- tremv_run.sh
+++ tremv_run.sh
@@ -6,6 +6,6 @@ else
 		screen -S "tremv_logger$1" -dm python3 tremv_logger.py
 	fi
 	if ! screen -list | grep -q "tremv_server$2"; then
-		screen -S "tremv_server$2" -dm python3 tremv_server.py $1
+		screen -S "tremv_server$1" -dm python3 tremv_server.py $1
 	fi
 fi
