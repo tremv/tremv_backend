315450d4c2c119ae643ec62b6e5de5eece65896a
Thordur Agust <ThordurKa@vedur.is>
2021-08-30 10:59:50 +0000

    added back the _read function in the config class

--- config.py
+++ config.py
@@ -5,17 +5,20 @@ class config:
     def __init__(self):
         self.filename = "config.json"
         self.stamp = 0
-
-        config_file = open(self.filename, "r")
-        self.config = json.loads(config_file.read())
-        config_file.close()
+        self.config = None
+        self._read()
 
     def __getitem__(self, key):
         return self.config[key]
+
+    def _read():
+        config_file = open(self.filename, "r")
+        self.config = json.loads(config_file.read())
+        config_file.close()
 
     def reload():
         stamp = os.stat(self.filename).st_mtime
 
         if(stamp != self.stamp):
-            self.read()
+            self._read()
             self.stamp = stamp
