9fa75139b08542b99a1255cbd760efa24234a5d5
Thordur Agust <ThordurKa@vedur.is>
2021-08-31 12:33:21 +0000

    self

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ class config:
     def __getitem__(self, key):
         return self.config[key]
 
-    def _read():
+    def _read(self):
         config_file = open(self.filename, "r")
         self.config = json.loads(config_file.read())
         config_file.close()
 
-    def reload():
+    def reload(self):
         stamp = os.stat(self.filename).st_mtime
 
         if(stamp != self.stamp):
